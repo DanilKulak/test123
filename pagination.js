447efd0f2e351da9201a2d2099ba3743d2f516af
DanilKulak <1998kylak@gmail.com>
2023-12-08 20:03:42 +0200

    done

--- pagination.js
+++ pagination.js
@@ -1,64 +1,80 @@
-// selecting required element
 const element = document.querySelector(".pagination ul");
-let totalPages = 20;
-let page = 10;
+let totalPages = 8;
+let page = 1;
 
-//calling function with passing parameters and adding inside element which is ul tag
 element.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
+
+function createPagination(totalPages, page) {
   let liTag = '';
   let active;
   let beforePage = page - 1;
   let afterPage = page + 1;
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+
+  if (page > 1) {
+    liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span>&lt;</span></li>`;
+  } else {
+    liTag += `<li class="btn prev disabled"><span>&lt;</span></li>`;
   }
 
-  if(page > 2){ //if page value is less than 2 then add 1 after the previous button
+  if (page > 2) {
     liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
-    if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
+    if (page > 3) {
       liTag += `<li class="dots"><span>...</span></li>`;
     }
   }
 
-  // how many pages or li show before the current li
   if (page == totalPages) {
     beforePage = beforePage - 2;
   } else if (page == totalPages - 1) {
     beforePage = beforePage - 1;
   }
-  // how many pages or li show after the current li
+
   if (page == 1) {
     afterPage = afterPage + 2;
   } else if (page == 2) {
-    afterPage  = afterPage + 1;
+    afterPage = afterPage + 1;
   }
 
   for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
+    if (plength > totalPages) {
       continue;
     }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
+    if (plength == 0) {
       plength = plength + 1;
     }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
+    if (page == plength) {
       active = "active";
-    }else{ //else leave empty to the active variable
+    } else {
       active = "";
     }
     liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
   }
 
-  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+  if (page < totalPages - 1) {
+    if (page < totalPages - 2) {
       liTag += `<li class="dots"><span>...</span></li>`;
     }
     liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
   }
 
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+  if (page < totalPages) {
+    liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>&gt;</span></li>`;
+  } else {
+    liTag += `<li class="btn next disabled"><span>&gt;</span></li>`;
   }
-  element.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
+
+  element.innerHTML = liTag;
+
+  // Додаємо обробники подій тільки для активних кнопок
+  const activeButtons = document.querySelectorAll('.pagination li:not(.disabled)');
+  activeButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      // Ваш код обробки події тут
+      // Наприклад, ви можете вивести номер сторінки або викликати інші функції
+      console.log(`Ви вибрали сторінку: ${event.currentTarget.textContent}`);
+    });
+  });
+
+  return liTag;
 }
+
